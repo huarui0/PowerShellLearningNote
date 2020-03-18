5416aecfc2679a3cc02ee9363c1e1f1a1ecd3726
wwanglai <wwanglai@163.com>
2020-03-18 15:10:08 +0800

    Add delete folder function...

--- ScriptForAndroid/Test_Generate_FolderTool.ps1
+++ ScriptForAndroid/Test_Generate_FolderTool.ps1
@@ -8,7 +8,7 @@ GetSourceFolders foreach ($item in $collection) {
 }
 # 1. 只拷贝文件夹，不包括文件夹下的内容
 方法：
-1. 拷贝完，更名
+1. 拷贝完，更名: Rename a directory: Rename-Item -Path c:\a\cc -NewName dd
 2. 根据源文件夹
 #>
 function CreateDestinationEmptyFolder {
@@ -211,4 +211,91 @@ function CopySourceFolderToDestination {
 }
 
 # 正式
-CopySourceFolderToDestination -SourceFolder "E:\AndroidDev\AndroidStudioProjects\Studies_Template\SunShineX" -DistinationParentFolder "E:\Notes\4_LearningNotes\PowerShellLearningNote\ScriptForAndroid\TestFolder\udlocal-Sunshine" -ForceOverride:$true -NewDistinationFolderName "S05.01-Solution-AsyncTaskLoader"
+# CopySourceFolderToDestination -SourceFolder "E:\AndroidDev\AndroidStudioProjects\Studies_Template\SunShineX" -DistinationParentFolder "E:\Notes\4_LearningNotes\PowerShellLearningNote\ScriptForAndroid\TestFolder\udlocal-Sunshine" -ForceOverride:$true -NewDistinationFolderName "S05.01-Solution-AsyncTaskLoader"
+
+
+
+
+
+
+function DeleteDesignatedFolderContents {
+    <#
+    .SYNOPSIS
+        实现：
+        1. 将源文件夹拷贝到指定的目标文件夹下（在目标文件夹下创建了新的文件夹）。
+        2. 拷贝空的源文件夹（即：文件夹内容为空），然后将模板文件夹下的内容拷贝到该文件夹下。
+    .PARAMETER SourceFolder
+        The file path of the text file you'd like to perform a find/replace on.
+    .PARAMETER DistinationParentFolder
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'NewFolder')]
+    [OutputType()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
+        [string]$DesignatedFolder, # 指要删除内容的文件夹
+        [Parameter()]
+        [string]$DesignatedFolderName, #
+        [Parameter()]
+        [string]$FolderContents, # *：说明是全部，FolderName：仅删除指定的文件夹
+        [Parameter(Mandatory=$false)]
+        [Switch]
+        $isDeleteFolderContents,
+        [Parameter(Mandatory=$false)]
+        [Switch]
+        $ForceDelete
+    )
+
+
+        # [Parameter(Mandatory = $true)]
+        # [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
+        # [string[]]$SourceFolders,
+
+
+    begin {
+        # $Find = [regex]::Escape($Find)
+        # 注：只是 $Find 用正则表达式，$Replace 一定是正确的数据，所以不可能是正则表达表达式
+        # $Replace = [regex]::Escape($Replace)
+    }
+
+    process {
+        try {
+            # 如果要拷贝的源文件夹所在的目标父文件夹存在
+            if (Test-Path -Path $DesignatedFolder -PathType 'Container') {
+
+                if ($isDeleteFolderContents) { # 仅删除文件夹的内容
+                    $FolderContents = Join-String -Separator '\' -InputObject $DesignatedFolder, "*"
+
+                    Remove-Item -Path $FolderContents  -Recurse  -Force
+
+                    # 有问题，不能用
+                    # Get-ChildItem -Path $DesignatedFolder -Recurse | Remove-Item $_ -Recurse -Force
+
+<# 有问题，不能用
+                    Get-ChildItem -Path $DesignatedFolder -Recurse | ForEach-Object -Parallel {
+                        Remove-Item -Path $_  -Recurse  -Force
+                    } -ThrottleLimit 4
+#>
+                } else { # 删除整个文件夹
+
+                    if ($ForceDelete) {
+                        Remove-Item -Path $DesignatedFolder -Recurse -Force
+                    } else {
+                        Write-Warning "The folder at '$DesignatedFolder' exists files or subfolders and the -ForceDelete param was not used"
+                    }
+
+                }
+
+            } else {
+                 Write-Host "目标父文件夹不存在: $DistinationParentFolder ..." -ForegroundColor Red
+            }
+
+
+        } catch {
+            Write-Error $_.Exception.Message
+        }
+    }
+}
+
+# 正式
+DeleteDesignatedFolderContents -DesignatedFolder "E:\Notes\4_LearningNotes\PowerShellLearningNote\ScriptForAndroid\TestFolder\udlocal-Sunshine\S05.01-Solution-AsyncTaskLoader\app\src\main" -isDeleteFolderContents:$true -ForceDelete:$true
