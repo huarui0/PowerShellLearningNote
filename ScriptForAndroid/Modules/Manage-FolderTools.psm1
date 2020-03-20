30dfdb78db9d2d51ebb7e2a01ddff9614b7d0852
wwanglai <wwanglai@163.com>
2020-03-20 23:04:48 +0800

    Add Manage-FolderTools.psm1 ...

--- ScriptForAndroid/Modules/Manage-FolderTools.psm1
+++ ScriptForAndroid/Modules/Manage-FolderTools.psm1
@@ -1,17 +1,15 @@
 ## Script: Test_Generate_FolderTool.ps1
 # 
 
-Import-Module -Name E:\Notes\4_LearningNotes\PowerShellLearningNote\ScriptForAndroid\Modules\Manage-FolderTools.psm1 -Verbose
-
 <#
-    GetTemplateFolder
-    GetSourceFolders foreach ($item in $collection) {
-        Copy-Template-Folder to each Destination
-    }
-    # 1. 只拷贝文件夹，不包括文件夹下的内容
-    方法：
-    1. 拷贝完，更名: Rename a directory: Rename-Item -Path c:\a\cc -NewName dd
-    2. 根据源文件夹
+ GetTemplateFolder
+GetSourceFolders foreach ($item in $collection) {
+    Copy-Template-Folder to each Destination
+}
+# 1. 只拷贝文件夹，不包括文件夹下的内容
+方法：
+1. 拷贝完，更名: Rename a directory: Rename-Item -Path c:\a\cc -NewName dd
+2. 根据源文件夹
 #>
 function New-DestinationEmptyFolder {
     <#
@@ -115,6 +113,81 @@ function New-DestinationEmptyFolder {
 # 1. 测试 - 在目的父文件夹下创建空的文件夹
 # CreateDestinationEmptyFolder -SourceFolder "E:\AndroidDev\AndroidStudioProjects\Studies&Practices\01_courses_Code\ud851-Sunshine\S05.01-Solution-AsyncTaskLoader" -DistinationParentFolder "E:\Notes\4_LearningNotes\PowerShellLearningNote\ScriptForAndroid\TestFolder\udlocal-Sunshine" -ForceOverride:$true
 
+function Copy-FolderContents {
+    <#
+    .SYNOPSIS
+        实现：
+        1. 将源文件夹拷贝到指定的目标文件夹下（在目标文件夹下创建了新的文件夹）。
+        2. 拷贝空的源文件夹（即：文件夹内容为空），然后将模板文件夹下的内容拷贝到该文件夹下。
+    .PARAMETER SourceFolder
+        The file path of the text file you'd like to perform a find/replace on.
+    .PARAMETER DistinationParentFolder
+    #>
+    [CmdletBinding()]
+    [OutputType()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
+        [string]$DistinationParentFolder,
+        [Parameter()]
+        [string]$DistinationFolder, # 在DistinationParentFolder下加NewDistinationFolderName
+        [Parameter()]
+        [string]$NewDistinationFolderName,
+        [Parameter()]
+        [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
+        [string]$SourceFolder,
+        [Parameter()]
+        [string]$SourceFolderName,
+        [Parameter()]
+        [string]$SourceFolderContents,
+        [Parameter(Mandatory=$false)]
+        [Switch]
+        $isCopyFolderContents,
+        [Parameter(Mandatory=$false)]
+        [Switch]
+        $ForceOverride
+    )
+
+    begin {
+        # $Find = [regex]::Escape($Find)
+        # 注：只是 $Find 用正则表达式，$Replace 一定是正确的数据，所以不可能是正则表达表达式
+        # $Replace = [regex]::Escape($Replace)
+    }
+
+    process {
+        try {
+
+            # 因为如果是拷贝成新的文件夹名，不用isCopyFolderContents 也同样，所有放这里。
+            $SourceFolderContents = Join-String -Separator '\' -InputObject $SourceFolder, "*"
+
+            if ($isCopyFolderContents) {
+
+                # $SourceFolderContents = Join-String -Separator '\' -InputObject $SourceFolder, "*"
+
+                # 将文件夹的所有内容拷贝到目标文件夹下
+                Copy-Item $SourceFolderContents $DistinationFolder -Recurse -Force
+
+            } else { # 如果是Copy后，更名为新的文件夹名，则需要用$isCopyFolderContents里的方法
+                if ($NewDistinationFolderName) {
+                    # 将文件夹的所有内容拷贝到目标文件夹下
+                    Copy-Item $SourceFolderContents $DistinationFolder -Recurse -Force
+                } else {
+                    # 啰嗦，不要
+                    # $SourceFolderContents = $SourceFolder
+                    # Copy-Item $SourceFolderContents $DistinationParentFolder -Recurse -Force
+
+                    # 将文件夹的所有内容拷贝到目标文件夹下 - 注意直接将源文件夹拷贝的父文件夹，即可！
+                    Copy-Item $SourceFolder $DistinationParentFolder -Recurse -Force
+                }
+            }
+
+        } catch {
+            Write-Error $_.Exception.Message
+        }
+    }
+
+
+}
 
 function Copy-SourceFolderToDestination {
     <#
@@ -151,7 +224,6 @@ function Copy-SourceFolderToDestination {
         $ForceOverride
     )
 
-
         # [Parameter(Mandatory = $true)]
         # [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
         # [string[]]$SourceFolders,
@@ -165,82 +237,6 @@ function Copy-SourceFolderToDestination {
 
     process {
 
-        function script:Sub_CopySourceFolderToDestination {
-            <#
-            .SYNOPSIS
-                实现：
-                1. 将源文件夹拷贝到指定的目标文件夹下（在目标文件夹下创建了新的文件夹）。
-                2. 拷贝空的源文件夹（即：文件夹内容为空），然后将模板文件夹下的内容拷贝到该文件夹下。
-            .PARAMETER SourceFolder
-                The file path of the text file you'd like to perform a find/replace on.
-            .PARAMETER DistinationParentFolder
-            #>
-            [CmdletBinding()]
-            [OutputType()]
-            param (
-                [Parameter(Mandatory = $true)]
-                [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
-                [string]$DistinationParentFolder,
-                [Parameter()]
-                [string]$DistinationFolder, # 在DistinationParentFolder下加NewDistinationFolderName
-                [Parameter()]
-                [string]$NewDistinationFolderName,
-                [Parameter()]
-                [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
-                [string]$SourceFolder,
-                [Parameter()]
-                [string]$SourceFolderName,
-                [Parameter()]
-                [string]$SourceFolderContents,
-                [Parameter(Mandatory=$false)]
-                [Switch]
-                $isCopyFolderContents,
-                [Parameter(Mandatory=$false)]
-                [Switch]
-                $ForceOverride
-            )
-        
-            begin {
-                # $Find = [regex]::Escape($Find)
-                # 注：只是 $Find 用正则表达式，$Replace 一定是正确的数据，所以不可能是正则表达表达式
-                # $Replace = [regex]::Escape($Replace)
-            }
-        
-            process {
-                try {
-        
-                    # 因为如果是拷贝成新的文件夹名，不用isCopyFolderContents 也同样，所有放这里。
-                    $SourceFolderContents = Join-String -Separator '\' -InputObject $SourceFolder, "*"
-        
-                    if ($isCopyFolderContents) {
-        
-                        # $SourceFolderContents = Join-String -Separator '\' -InputObject $SourceFolder, "*"
-        
-                        # 将文件夹的所有内容拷贝到目标文件夹下
-                        Copy-Item $SourceFolderContents $DistinationFolder -Recurse -Force
-        
-                    } else { # 如果是Copy后，更名为新的文件夹名，则需要用$isCopyFolderContents里的方法
-                        if ($NewDistinationFolderName) {
-                            # 将文件夹的所有内容拷贝到目标文件夹下
-                            Copy-Item $SourceFolderContents $DistinationFolder -Recurse -Force
-                        } else {
-                            # 啰嗦，不要
-                            # $SourceFolderContents = $SourceFolder
-                            # Copy-Item $SourceFolderContents $DistinationParentFolder -Recurse -Force
-        
-                            # 将文件夹的所有内容拷贝到目标文件夹下 - 注意直接将源文件夹拷贝的父文件夹，即可！
-                            Copy-Item $SourceFolder $DistinationParentFolder -Recurse -Force
-                        }
-                    }
-        
-                } catch {
-                    Write-Error $_.Exception.Message
-                }
-            }
-        
-        
-        }
- 
         try {
             # 如果要拷贝的源文件夹所在的目标父文件夹存在
             if (Test-Path -Path $DistinationParentFolder -PathType 'Container') {
@@ -308,7 +304,6 @@ function Copy-SourceFolderToDestination {
 }
 
 
-
 # 2. 测试 - 将文件夹Copy到ParentFolder下，并更名 - 更名不能直接拷贝文件夹，用 -isCopyFolderContents 的方法 -- 不用也可以，但要有 -NewDistinationFolderName
 # CopySourceFolderToDestination -SourceFolder "E:\AndroidDev\AndroidStudioProjects\Studies_Template\SunShineX" -DistinationParentFolder "E:\Notes\4_LearningNotes\PowerShellLearningNote\ScriptForAndroid\TestFolder\udlocal-Sunshine" -ForceOverride:$true -NewDistinationFolderName "S05.01-Solution-AsyncTaskLoader" -isCopyFolderContents:$true
 
@@ -318,7 +313,7 @@ function Copy-SourceFolderToDestination {
 # 正式-第二种 - 拷贝内容到文件夹下（不更名），-正确，注意：DistinationParentFolder都是ParentFolder
 # CopySourceFolderToDestination -SourceFolder "E:\AndroidDev\AndroidStudioProjects\Studies&Practices\01_courses_Code\ud851-Sunshine\S05.01-Solution-AsyncTaskLoader\app\src\main" -DistinationParentFolder "E:\Notes\4_LearningNotes\PowerShellLearningNote\ScriptForAndroid\TestFolder\udlocal-Sunshine\S05.01-Solution-AsyncTaskLoader\app\src" -ForceOverride:$true -isCopyFolderContents:$true
 
-function DeleteDesignatedFolderContents {
+function Remove-DesignatedFolderContents {
     <#
     .SYNOPSIS
         实现：
@@ -400,159 +395,4 @@ function DeleteDesignatedFolderContents {
 # 正式
 # DeleteDesignatedFolderContents -DesignatedFolder "E:\Notes\4_LearningNotes\PowerShellLearningNote\ScriptForAndroid\TestFolder\udlocal-Sunshine\S05.01-Solution-AsyncTaskLoader\app\src\main" -isDeleteFolderContents:$true -ForceDelete:$true
 
-
-function Batch_CopySourceFolderToDestination {
-    <#
-    .SYNOPSIS
-        实现在指定的文件或批量文件（名称相同）中，查找和替换文本的功能。
-    .PARAMETER FilePaths
-        The file path of the text file you'd like to perform a find/replace on.
-    .PARAMETER FilePath
-        The file path of the text file you'd like to perform a find/replace on.
-        .PARAMETER OriginalFilePath
-        该变量 用来实现将源文件（原始文件），覆盖到指定的文件夹中相同名称的文件，根据 $Force 变量判断是否覆盖原文件。
-
-    .PARAMETER FileName
-        The file name of the text file you'd like to perform a find/replace on.
-    .PARAMETER Find
-        The string you'd like to replace.
-    .PARAMETER Replace
-        The string you'd like to replace your 'Find' string with.
-    .PARAMETER NewFilePath
-        If a new file with the replaced the string needs to be created instead of replacing
-        the contents of the existing file use this param to create a new file.
-    .PARAMETER Force
-        If the NewFilePath param is used using this param will overwrite any file that
-        exists in NewFilePath.
-    #>
-    [CmdletBinding()]
-    [OutputType()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
-        [string]$DistinationParentFolder,
-        [Parameter()]
-        [string]$DistinationFolder, # 在DistinationParentFolder下加NewDistinationFolderName
-        [Parameter()]
-        [string]$NewDistinationFolderName,
-        [Parameter(Mandatory = $true)]
-        [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
-        [string]$SourceParentFolder,
-        [Parameter()]
-        [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
-        [string]$SourceFolder,
-        [Parameter()]
-        [string]$SourceFolderName,
-        [Parameter()]
-        [string]$SourceFolderContents,
-        [Parameter(Mandatory = $true)]
-        [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
-        [string]$TemplateFolder,
-        [Parameter(Mandatory=$false)]
-        [Switch]
-        $isCopyFolderContents,
-        [Parameter(Mandatory=$false)]
-        [Switch]
-        $ForceOverride
-    )
-
-
-
-    begin {
-        # $Find = [regex]::Escape($Find)
-        # 注：只是 $Find 用正则表达式，$Replace 一定是正确的数据，所以不可能是正则表达表达式
-        # $Replace = [regex]::Escape($Replace)
-    }
-
-    process {
-        try {
-            Write-Host "`$DistinationParentFolder = $DistinationParentFolder"
-
-            # 不能用 -Recurse
-            Get-ChildItem -Path $SourceParentFolder | Where-Object { $_.PSIsContainer -eq $true } | ForEach-Object -Parallel {
-                # "$using:Find $_"
-                Write-Host "`$SourceParentFolder = $using:SourceParentFolder"
-                Write-Host "`$_ = $_" -ForegroundColor Blue
-
-                $NewDistinationFolderName =  Split-Path -Path $_ -Leaf 
-
-# 步骤
-# 1. 测试 - 在目的父文件夹下创建空的文件夹
-# CreateDestinationEmptyFolder -SourceFolder "E:\AndroidDev\AndroidStudioProjects\Studies&Practices\01_courses_Code\ud851-Sunshine\S05.01-Solution-AsyncTaskLoader" -DistinationParentFolder "E:\Notes\4_LearningNotes\PowerShellLearningNote\ScriptForAndroid\TestFolder\udlocal-Sunshine" -ForceOverride:$true
-
-# 2. 测试 - 将文件夹Copy到ParentFolder下，并更名 - 更名不能直接拷贝文件夹，用 -isCopyFolderContents 的方法 -- 不用也可以，但要有 -NewDistinationFolderName
-                Copy-SourceFolderToDestination -TemplateFolder $TemplateFolder -SourceParentFolder $SourceParentFolder -DistinationParentFolder $DistinationParentFolder -ForceOverride:$false -NewDistinationFolderName $NewDistinationFolderName -isCopyFolderContents:$true
-                # 测试的方法：先建一个文件夹，测试，会不会覆盖。结果：必须不会覆盖
-            } -ThrottleLimit 4
-
-        } catch {
-            Write-Error $_.Exception.Message
-        }
-    }
-}
-
-
-
-Batch_CopySourceFolderToDestination -TemplateFolder "E:\AndroidDev\AndroidStudioProjects\Studies_Template\SunShineX" -SourceParentFolder "E:\AndroidDev\AndroidStudioProjects\Studies&Practices\01_courses_Code\ud851-Sunshine" -DistinationParentFolder "E:\Notes\4_LearningNotes\PowerShellLearningNote\ScriptForAndroid\TestFolder\udlocal-Sunshine" -ForceOverride:$true -NewDistinationFolderName $NewDistinationFolderName -isCopyFolderContents:$true
-
-function Batch_FindandReplace_InTextFile {
-    <#
-    .SYNOPSIS
-        实现在指定的文件或批量文件（名称相同）中，查找和替换文本的功能。
-    .PARAMETER FilePaths
-        The file path of the text file you'd like to perform a find/replace on.
-    .PARAMETER FilePath
-        The file path of the text file you'd like to perform a find/replace on.
-        .PARAMETER OriginalFilePath
-        该变量 用来实现将源文件（原始文件），覆盖到指定的文件夹中相同名称的文件，根据 $Force 变量判断是否覆盖原文件。
-
-    .PARAMETER FileName
-        The file name of the text file you'd like to perform a find/replace on.
-    .PARAMETER Find
-        The string you'd like to replace.
-    .PARAMETER Replace
-        The string you'd like to replace your 'Find' string with.
-    .PARAMETER NewFilePath
-        If a new file with the replaced the string needs to be created instead of replacing
-        the contents of the existing file use this param to create a new file.
-    .PARAMETER Force
-        If the NewFilePath param is used using this param will overwrite any file that
-        exists in NewFilePath.
-    #>
-    [CmdletBinding()]
-    [OutputType()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [ValidateScript({Test-Path -Path $_ -PathType 'Any'})]
-        [string[]]$FilePaths,
-        [Parameter()]
-        [string]$FilePath,
-        [Parameter(Mandatory = $true)]
-        [string]$FileName,
-        [Parameter(Mandatory = $true)]
-        [string]$Find
-    )
-    begin {
-
-        # 注：只是 $Find 用正则表达式，$Replace 一定是正确的数据，所以不可能是正则表达表达式
-        # $Replace = [regex]::Escape($Replace)
-    }
-
-    process {
-        try {
-
-            $FileEntries = $FilePaths | ForEach-Object -Parallel {
-                Write-Host  "Output: $_"
-                $FilePath = $_
-                Write-Host "第一层循环`$FilePath = $FilePath"
-                # Start-Sleep 1
-                # $FileCount = $FilePaths.Count
-                # $FileCount
-
-            }  -ThrottleLimit 4
-
-        } catch {
-            Write-Error $_.Exception.Message
-        }           
-    }
-}
+Export-ModuleMember -Function New-DestinationEmptyFolder, Copy-FolderContents, Copy-SourceFolderToDestination, Remove-DesignatedFolderContents
